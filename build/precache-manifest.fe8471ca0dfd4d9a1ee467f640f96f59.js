5d499db21b9fdd0b3ba9b46c7c25b40e0ca0c1b6
ankitnathtiwari <ankitfounder@gmail.com>
2020-11-06 19:12:20 +0530

    with build

---
+++ build/precache-manifest.fe8471ca0dfd4d9a1ee467f640f96f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9400713736a7a9d77c45978bd922b03d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d19b1597066a06f6310",
+    "url": "/static/css/main.c4d81d7f.chunk.css"
+  },
+  {
+    "revision": "ba1f18c3d3fbe899801b",
+    "url": "/static/js/2.1f452f02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f452f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d19b1597066a06f6310",
+    "url": "/static/js/main.5a83e743.chunk.js"
+  },
+  {
+    "revision": "afdf77d3b457fc16c08b",
+    "url": "/static/js/runtime-main.88257cd8.js"
+  },
+  {
+    "revision": "dccbe6b88570f7d925316024737025ee",
+    "url": "/static/media/landing_pic.dccbe6b8.jpg"
+  }
+]);
